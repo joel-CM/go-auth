04001b2e7225671aebf1e769133ff8f97c3422e4
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2023-09-11 00:10:57 -0300

    add field validations

--- app/models/user.go
+++ app/models/user.go
@@ -11,6 +11,6 @@ type User struct {
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
-	Name      string         `json:"name"`
-	Email     string         `json:"email"`
+	Name      string         `json:"name" validate:"required"`
+	Email     string         `json:"email" validate:"required,email"`
 }
