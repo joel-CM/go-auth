ba6367ce4d3e11da9c276515601dd4f10fc07c66
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2023-09-12 14:32:40 -0300

    config godotenv

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"github.com/joel-CM/go-auth/app/db"
 	"github.com/joel-CM/go-auth/app/models"
 	"github.com/joel-CM/go-auth/app/routes"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	app := gin.Default()
