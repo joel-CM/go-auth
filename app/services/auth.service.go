dcc02b6ecd6cc57a37f601451bfbe5de74e5b6a4
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2023-09-12 17:20:34 -0300

    add verifyToken function

--- app/services/auth.service.go
+++ app/services/auth.service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -11,4 +12,19 @@ func CreateToken(user models.UserSignInModel) (string, error) {
 	claims := jwt.MapClaims{"email": user.Email}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(os.Getenv("JWT_SECURE_KEY")))
+}
+
+func VerifyToken(tokenString string) (bool, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("invalid token")
+		}
+		return []byte(os.Getenv("JWT_SECURE_KEY")), nil
+	})
+
+	if err != nil {
+		return false, err
+	}
+
+	return token.Valid, nil
 }
