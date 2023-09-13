3cba4d1bc9e63a770bd9ab03e0ab745af1c88a4c
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2023-09-13 00:17:17 -0300

    add resource route

--- app/routes/auth.route.go
+++ app/routes/auth.route.go
@@ -2,9 +2,13 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/joel-CM/go-auth/app/controller"
+	"github.com/joel-CM/go-auth/app/middlewares"
 )
 
 func AuthRoute(app *gin.Engine) {
-	// api := app.Group("/api")
-	// auth := api.Group("/auth")
+	api := app.Group("/api")
+	auth := api.Group("/auth", middlewares.AuthMiddleware())
+
+	auth.GET("/resource", controller.GetResource)
 }
